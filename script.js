ef6753277b5e43c3df2930e18e90be0a3f75a2ce
Arek <majkhrhr@gmail.com>
2019-01-25 00:46:18 +0100

    js refactoring, ternary operator

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const selectCards = document.querySelectorAll(".card-component");
+//add event listener to each card
+selectCards.forEach(card => card.addEventListener("click", flipCard));
 
-//if first or second card was flipped
 let isFlipped = false;
 let firstCard, secondCard;
-//add event listener to each card
+
 function flipCard() {
   // 'this' represents the element that started the listenerEvent - a card
-
   // console.log(this);
   this.classList.add("toggle-flip");
-
+  //if first or second card was flipped
   if (!isFlipped) {
     // first card clicked
     isFlipped = true;
@@ -24,19 +24,31 @@ function flipCard() {
     console.log(firstCard.dataset.match);
     console.log(secondCard.dataset.match);
 
-    if (firstCard.dataset.match === secondCard.dataset.match) {
-      //remove listeners to avoid them being clicked again
-      firstCard.removeEventListener("click", flipCard);
-      secondCard.removeEventListener("click", flipCard);
-    } else {
-      // flip back cards if its not a match by removing toggle-flip class
-      //set timeut to see second click
-      setTimeout(() => {
-        firstCard.classList.remove("toggle-flip");
-        secondCard.classList.remove("toggle-flip");
-      }, 900);
-    }
     console.log("listeners removed from items already clicked");
+    verifyCardMatch();
   }
 }
-selectCards.forEach(card => card.addEventListener("click", flipCard));
+/*ternary operator(operator waronkowy, can be used below) */
+// checks for card match
+function verifyCardMatch() {
+  if (firstCard.dataset.match === secondCard.dataset.match) {
+    //remove listeners to avoid them being clicked again
+    disableCardListeners();
+  } else {
+    unflip();
+  }
+}
+
+function disableCardListeners() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+}
+
+function unflip() {
+  // flip back cards if its not a match by removing toggle-flip class
+  //set timeut to see second click
+  setTimeout(() => {
+    firstCard.classList.remove("toggle-flip");
+    secondCard.classList.remove("toggle-flip");
+  }, 500);
+}
